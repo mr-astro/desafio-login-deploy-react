a34ade10677d2bfd7b395fa0359aba1a44b2e0ee
Daniel Canales <xastrox@gmail.com>
2021-09-02 19:26:25 -0400

    login operativo

--- src/container/login/Login.jsx
+++ src/container/login/Login.jsx
@@ -1,10 +1,61 @@
+import React, {useState, useCallback} from 'react'
+import { useDispatch } from 'react-redux'
+import {userLogin} from '../../store/login/action'
+import { useHistory } from 'react-router-dom'
+
 const Login = () => {
+    const [form, setForm] = useState({
+        username:'',
+        password:''
+    })
+
+    const dispatch = useDispatch()
+    const history = useHistory()
+    const handleInputChange = (event) => {
+        const target = event.target
+        setForm({
+            ...form,
+            [target.name]:target.value
+        })
+    }
+
+    const handleSubmit = useCallback((event) => {
+        event.preventDefault()
+        dispatch(userLogin(form))
+        history.push('/users')
+    },
+    [dispatch, form, history])
+
     return (
-        <div>
+        <div className="container">
             <h1>Login</h1>
-            <input type="text" />
-            <input type="text" />
-            <button>Ingresar</button>
+            <form noValidate autoComplete='off' onSubmit={handleSubmit}>
+                <div className='card'>
+                    <input 
+                    id='username'
+                    name='username'
+                    className='form-control username'
+                    placeholder='Usuario'
+                    onChange={handleInputChange}
+                    value={form.username} />
+                </div>
+                <div className='card'>
+                    <input
+                    id='password'
+                    name='password'
+                    className='form-control'
+                    placeholder='Password'
+                    type='password'
+                    onChange={handleInputChange}
+                    value={form.password}/>
+                </div>
+                <div className='card'>
+                    <button
+                    id='buttonSubmit'
+                    className='btn btn-primary'
+                    type='submit'>Ingresar</button>
+                </div>
+            </form>
         </div>
     )
 }
